4eab69fcbf95eed9765a55efce4ba574e10e4bf1
SaraCS21 <saracs15.scssn@gmail.com>
2022-10-03 23:08:15 +0100

    Primer ejercicio terminado

--- JavaScript_03-10-2022/arrays.js
+++ JavaScript_03-10-2022/arrays.js
@@ -7,14 +7,8 @@ const mostrar_elementos = (array) => array.join(", ");
 // Mostrar elementos del array sentido inverso
 const mostrar_elementos_inverso = (array) => array.reverse().join(", ");
 
-array = ["a", 2, "b", 5];
-console.log(mostrar_elementos_inverso(array));
-
 // Mostrar elementos ordenados alfabéticamente
-// const mostrar_alfabeticamente = (array) => array.sort().forEach(elemento => console.log(elemento));
-function mostrar_alfabeticamente(array){
-
-}
+const mostrar_alfabeticamente = (array) => array.sort().join(", ");
 
 // Añade un elemento al principio
 const anadir_principio = (array, valor) => array.unshift(valor);
@@ -22,9 +16,17 @@ const anadir_principio = (array, valor) => array.unshift(valor);
 // Añade un elemento al final
 const anadir_final = (array, valor) => array.push(valor);
 
-// Eliminar al principio (Falta mostrarlo)
+// Eliminar al principio
 const eliminar_principio = array => array.shift();
 
-// Eliminar al final (Falta mostrarlo)
+// Eliminar al final
 const eliminar_final = array => array.pop();
 
+// Mostrar elemento por posición
+const mostrar_elemento = (array, pos) => array[pos];
+
+// Mostrar posición de elemento
+const mostrar_posicion = (array, elemento) => array.indexOf(elemento);
+
+// Mostrar elementos en un intervalo
+const mostrar_intervalo = (array, inicio, fin) => array.slice(inicio, fin)
