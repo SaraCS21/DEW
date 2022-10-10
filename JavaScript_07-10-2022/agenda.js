0dcd7c9403f1b10de3128558f2527f99bd0190c8
Sara <saracs15.scss@gmail.com>
2022-10-10 14:15:01 +0100

    actividad agenda

--- JavaScript_07-10-2022/agenda.js
+++ JavaScript_07-10-2022/agenda.js
@@ -1,5 +1,3 @@
-
-
 class Agenda_Tareas{
     #tareas = [];
 
--- JavaScript_07-10-2022/menu.js
+++ JavaScript_07-10-2022/menu.js
@@ -2,6 +2,11 @@ import Tarea from "./tarea.js";
 import Agenda_Tareas from "./agenda.js";
 
 console.log(```
-
-
+    ¿Qué desea hacer?
+    1.- Añadir una nueva tarea
+    2.- Modificar una tarea
+    3.- Eliminar una tarea
+    4.- Eliminar todas las tareas hechas
+    5.- Mostrar todas las tareas
+    6.- 
 ```)
