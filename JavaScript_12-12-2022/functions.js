8d14efb1ff069cf787f690d53d9bb05149c2e03b
Sara <saracs15.scss@gmail.com>
2022-12-13 17:25:13 +0000

    actividad validaciones

--- JavaScript_12-12-2022/functions.js
+++ JavaScript_12-12-2022/functions.js
@@ -10,15 +10,13 @@ let validations = {
     "date": /(([1-9])|([1-2]\d)|(3[0-1]))\/(([1-9])|([1][0-2]))\/\d{4}/,
     "hour_12": /(([1][0-2])|\d):(([1-5]\d)|\d):(([1-5]\d)|\d)/,
     "hour_24": /(([2][0-4])|([1]\d)|\d):(([1-5]\d)|\d):(([1-5]\d)|\d)/,
-    "password": a,
-    "user_name": /[0-9a-z_-]+/,
-    "email": a,
-    "url": a,
-    "ipv4": a,
-    "color": /[#]{1}[A-F0-9]{6}/
+    "password": /(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%#?&])[A-Za-z\d@$!%*#?&]{10,}/,
+    "user_name": /[0-9a-z_-]{3, 16}/,
+    "email": /[a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}/,
+    "url": /http[s]?:\/{2}(www.)?\w+[.][A-Za-z]{2,3}\/?/,
+    "ipv4": /(((([2][0-4]\d)|(25[0-5]))|([1]\d{2})|\d{2}|\d)[.]){3}((([2][0-4]\d)|(25[0-5]))|([1]\d{2})|\d{2}|\d)/,
+    "color": /[#][A-Fa_f\d]{3,6}/
 }
-
-// con letras minúsculas, números, guion bajo y guion medio. 
 
 let body = document.querySelector("body");
 let result = "OK";
